e9ff275cf39ea8eec00903a6e28c68a61b3fa78c
Keram <keramh@azet.sk>
2021-11-01 14:07:00 +0100

    cv_03 driver shift register

---
+++ cv_03/Src/sct.h
@@ -0,0 +1,14 @@
+/*
+ * stc.h
+ *
+ *  Created on: 30. 10. 2021
+ *      Author: Marek
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_init(void);
+void sct_led(uint32_t value);
+
+#endif /* SCT_H_ */
