b13546665114b2036785fe621ffd711ff6f14929
Keram <keramh@azet.sk>
2021-11-15 22:10:01 +0100

    conversion on 7seg+LED

---
+++ cv_04/Core/Inc/sct.h
@@ -0,0 +1,15 @@
+/*
+ * stc.h
+ *
+ *  Created on: 30. 10. 2021
+ *      Author: Marek
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t, uint8_t led);
+
+#endif /* SCT_H_ */
